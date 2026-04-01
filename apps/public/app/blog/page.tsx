02ad531ec4ccbbdb4c1f67fb339d02f9fa5b4c53
Esteban <estebanvangi@icloud.com>
2026-04-01 13:48:11 +0200

    Refactor: change my blog to my portfolio

--- apps/public/app/blog/page.tsx
+++ apps/public/app/blog/page.tsx
@@ -2,14 +2,14 @@ import { BlogPosts } from "@/app/components/posts";
 
 
 export const metadata = {
-  title: "Blog",
-  description: "Read my blog.",
+  title: "Portfolio",
+  description: "View my portfolio.",
 };
 
 export default async function Page() {
   return (
     <section className="w-full h-full">
-      <h1 className="font-semibold text-2xl mb-8 ml-8 tracking-tighter">My Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 ml-8 tracking-tighter">My Portfolio</h1>
       {/* @ts-ignore */}
       <BlogPosts />
     </section>
